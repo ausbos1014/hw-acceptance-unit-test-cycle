070751734c425645c7f01a76cc03326d25699385
Austin L <alundgren@swbell.net>
2018-10-04 20:06:17 +0000

    Add director to existing movie - edit and fill in pass

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,3 +3,8 @@ Given(/the following movies exist:$/) do |movies_table|
         Movie.create!(movie)
     end
 end
+
+Then(/^the director of "(.*)" should be "(.*)"$/) do |movie_title, movie_director|
+    key=Movie.where(title: "#{movie_title}")
+    expect(key.director).to eq movie_director
+end
