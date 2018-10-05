df3815c2ee87b680f6b5edbdedbc3ad04446f819
Austin L <alundgren@swbell.net>
2018-10-05 05:29:06 +0000

    Created and successfully tested rspec to find movies with same director

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -2,7 +2,16 @@ require 'rails_helper'
 
 describe MoviesController do
     describe 'Find with Same Director' do
+            it "should find all movies with the same director" do
+            
+            @movie = Movie.create!(:title => "10 Things I Hate About You", :rating => "PG-13", :director => "Gil Junger", :release_date => Time.now)
         
-    
-    
+            expect(Movie).to receive(:find).with("#{@movie.id}").and_return(@movie)
+            expect(Movie).to receive(:where).with(:director => @movie.director)
+       
+            get :search_director, :id => @movie.id
+                    
+            expect(response).to render_template(:search_director)
+        end
+    end
 end
