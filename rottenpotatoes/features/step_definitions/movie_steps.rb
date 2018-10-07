d2027262b8f1ae59cccfef799409fa6ea876f728
ausbos1014 <alundgren@swbell.net>
2018-10-06 22:49:55 -0500

    Update movie_steps.rb

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -11,11 +11,3 @@ end
 When(/^I select checkbox "(.*?)"$/) do |cb|
   check(cb)
 end
-
-#Then(/^the director of "(.*)" should be "(.*)"$/) do |title, director_of_movie|
-#    step %Q{I should see "#{title_of_movie}"}
-#    step %Q{I should see "#{director_of_movie}"}
-#
-#    movie=Movie.where(Title: "#{movie_title}")
-#    expect(movie.director).to eq movie_director
-#end
